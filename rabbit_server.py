10872432356bcc89cd251ec68ebc175266b5fb08
stevendeporre123 <84781479+stevendeporre123@users.noreply.github.com>
2022-11-25 09:32:38 +0100

    try rabbitmq

---
+++ rabbit_server.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import pika
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='myrabbit'))
+channel = connection.channel()
+
+channel.queue_declare(queue='GR')
+
+channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
